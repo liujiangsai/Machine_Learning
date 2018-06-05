176441ce270d44d23d783fb4c1eeb15d491b843c
Jason Liu <jiangsailiu@gmail.com>
2018-06-05 16:38:49 +0800

    naive bayes for junk email

--- Bayes/bayes.py
+++ Bayes/bayes.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 import numpy as np
 from math import log
-
+import random
 
 """
 Description: ceate test dataset
@@ -29,41 +29,34 @@ Returns:
 """
 
 def createVocabList(dataSet):
-	vocabSet = set([])  					#创建一个空的不重复列表
-	for document in dataSet:				
-		vocabSet = vocabSet | set(document) #取并集
+	vocabSet = set([])
+	for data in dataSet:
+		vocabSet = vocabSet | set(data)
 	return list(vocabSet)
 
 def setOfWords2Vec(vocabList, inputVec):
 	returnVec = [0]*len(vocabList)
+	for word in inputVec:
+		if word in vocabList:
+			returnVec[vocabList.index(word)] = 1
+	return returnVec
+
+def bagOfWords2Vec(vocabList, inputVec):
+	returnVec = [0]*len(vocabList)
 	for i in range(len(inputVec)):
 		word = inputVec[i]
 		if word in vocabList:
-			returnVec[vocabList.index(word)] = 1
-		else:
-			print "word %s is not in my Vocabulary" % word
+			returnVec[vocabList.index(word)] += 1
 	return returnVec
 
-"""
-函数说明:朴素贝叶斯分类器训练函数
-
-Parameters:
-	trainMatrix - 训练文档矩阵，即setOfWords2Vec返回的returnVec构成的矩阵
-	trainCategory - 训练类别标签向量，即loadDataSet返回的classVec
-Returns:
-	p0Vect - 非的条件概率数组
-	p1Vect - 侮辱类的条件概率数组
-	pAbusive - 文档属于侮辱类的概率
-"""
-
-def trainNB0(trainMatrix,trainCategory):
-	numTrainDocs = len(trainMatrix)							#计算训练的文档数目
-	numWords = len(trainMatrix[0])	
+def trainNB(trainMatrix, trainCategory):
+	numTrainDocs = len(trainMatrix)
+	numWords = len(trainMatrix[0])
 	pAbusive = sum(trainCategory)/float(numTrainDocs)
-	p0Num = np.zeros(numWords)
-	p1Num = np.zeros(numWords)
-	p0Denom = 0.0
-	p1Denom = 0.0
+	p1Num = np.ones(numWords)
+	p0Num = np.ones(numWords)
+	p1Denom = 2.0
+	p0Denom = 2.0
 	for i in range(numTrainDocs):
 		if trainCategory[i] == 1:
 			p1Num += trainMatrix[i]
@@ -71,16 +64,65 @@ def trainNB0(trainMatrix,trainCategory):
 		else:
 			p0Num += trainMatrix[i]
 			p0Denom += sum(trainMatrix[i])
-	p1Vect = p1Num / p1Denom
-	p0Vect = p0Num / p0Denom
-	return p0Vect,p1Vect,pAbusive
+	p1Vec = np.log(p1Num / p1Denom)
+	p0Vec = np.log(p0Num / p0Denom)
+	return p0Vec,p1Vec,pAbusive
 
-def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
-	p1 = reduce(lambda x,y:x+y, vec2Classify * p1Vec) + log(pClass1)    			#对应元素相乘
-	p0 = reduce(lambda x,y:x+y, vec2Classify * p0Vec) + log(1.0 - pClass1)
-	print('p0:',p0)
-	print('p1:',p1)
-	if p1 > p0:
-		return 1
-	else: 
+def classifyNB(inputMatrix, p0Vec, p1Vec, pAbusive):
+	p0 = sum(inputMatrix*p0Vec) + log(1-pAbusive)
+	p1 = sum(inputMatrix*p1Vec) + log(pAbusive)
+	if p0 > p1:
 		return 0
+	else:
+		return 1
+
+def testingNB():
+	listOfPosts,listCalasses = loadDataSet()
+	myVocabList = createVocabList(listOfPosts)
+	trainMatrix = []
+	for postinDoc in listOfPosts:
+		trainMatrix.append(setOfWords2Vec(myVocabList, postinDoc))
+	p0V,p1V,pAb = trainNB(np.array(trainMatrix), np.array(listCalasses))
+	testEntry = ['love','dalmation']
+	thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+	print testEntry,'classified as:',classifyNB(thisDoc, p0V, p1V, pAb)
+
+def textParse(bigString):
+	import re
+	listOfTokens = re.split(r'\W*', bigString)
+	return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+def spamTest():
+	docList = []; classList = []
+	for i in range(1,26):
+		wordList = textParse(open('email/spam/%d.txt' % i, 'r').read())
+		docList.append(wordList)
+		classList.append(1)
+		wordList = textParse(open('email/ham/%d.txt' % i, 'r').read())
+		docList.append(wordList)
+		classList.append(0)
+	myVocabList = createVocabList(docList)
+	trainSet = list(range(50));testSet = []
+	for i in range(10):
+		randomIndex = int(random.uniform(0,len(trainSet)))
+		testSet.append(trainSet[randomIndex])
+		del(trainSet[randomIndex])
+	trainMatrix = []; trainCategory = []
+	for index in trainSet:
+		trainMatrix.append(setOfWords2Vec(myVocabList, docList[index]))
+		trainCategory.append(classList[index])
+	p0V,p1V,pAbusive = trainNB(np.array(trainMatrix), np.array(trainCategory))
+	errorCount = 0
+	for index in testSet:
+		testMatrix = setOfWords2Vec(myVocabList, docList[index])
+		testClass = classList[index]
+		if classifyNB(np.array(testMatrix), p0V, p1V, pAbusive) != testClass:
+			print 'wrong index:' + str(index)
+			print 'error text:',docList[index]
+			errorCount += 1
+	print 'total error rate %.2f%%' % (float(errorCount)/len(testSet)*100)
+
+
+if __name__ == '__main__':
+	for i in range(10):
+		spamTest()		
